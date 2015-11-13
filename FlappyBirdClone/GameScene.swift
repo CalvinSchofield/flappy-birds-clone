5612d0b92d5640f43af1d1b51e07b58f3be192fc
Calvin Schofield <CalvinJSchofield@gmail.com>
2015-11-13 08:58:18 -0600

    GameViewController.swift - Don't want to see the Frames Per Second and the Node Count right now, may change in the future when considering debugging. GameScene.swift - deleted default code and added simple code to border the phone with a physics body and add a node with a physics body

--- FlappyBirdClone/GameScene.swift
+++ FlappyBirdClone/GameScene.swift
@@ -8,38 +8,72 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+
+    var skyColor = SKColor(red: 250/255, green: 255/255, blue: 245/255, alpha: 1.0)
+    
+    var player = SKShapeNode()
+    
+    var gameStart = false
+    
+    var count = 0
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        self.backgroundColor = skyColor
+        
+        self.physicsWorld.contactDelegate = self
+        
+        physicsWorld.gravity = CGVectorMake(0, -20)
+        
+        let borderFrame = SKPhysicsBody.init(edgeLoopFromRect: frame)
+        
+        physicsBody = borderFrame
+        
+        player = SKShapeNode(circleOfRadius: 20)
+        
+        player.position = CGPoint(x: CGRectGetMidX(self.frame) - 50, y: CGRectGetMidY(self.frame))
+        
+        player.physicsBody = SKPhysicsBody(circleOfRadius: 20)
+        
+        if gameStart && count > 0 {
+        
+            player.physicsBody?.dynamic = true
+        
+            player.physicsBody?.allowsRotation = false
+            
+            gameStart = false
+            
+        } else {
+            
+            player.physicsBody?.dynamic = false
+            
+            player.physicsBody?.allowsRotation = false
+            
+            gameStart = true
+            
+        }
+        
+        addChild(player)
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        count++
+        
+        player.physicsBody?.velocity = CGVectorMake(0, 0)
+        
+        player.physicsBody?.applyImpulse(CGVectorMake(0, 55))
+        
+        
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    
 }
