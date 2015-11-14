27de87a0c9f49f1ccfa92a77666a33e0076bef75
Calvin Schofield <CalvinJSchofield@gmail.com>
2015-11-13 20:32:25 -0600

    First stab at SKActions to make the pipes move

--- FlappyBirdClone/GameScene.swift
+++ FlappyBirdClone/GameScene.swift
@@ -12,13 +12,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     var skyColor = SKColor(red: 180/255, green: 200/255, blue: 255/255, alpha: 1.0)
     
+    var moveNode = SKNode()
+    
     var ground = SKShapeNode()
     
-    var pipe = SKShapeNode()
+    var pipeDown = SKShapeNode()
+    
+    var pipeUp = SKShapeNode()
+    
+    var pipes = SKNode()
     
     var player = SKShapeNode()
     
     var movePipes = SKAction()
+    
+    var movePipesAndRemove = SKAction()
+    
+    let verticalPipeGap = 150.0
     
     var gameStarted = false
     
@@ -28,6 +38,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMoveToView(view: SKView) {
         //Setting up Scene
         self.backgroundColor = skyColor
+        
+        
+        //Preparing for Moving Methods
+        moveNode.addChild(pipes)
         
         
         //Setting up Ground
@@ -52,18 +66,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(ground)
         
         
-        //Setting up Pipes
-        pipe = SKShapeNode(rectOfSize: CGSize(width: 80, height: 800), cornerRadius: 10)
-        
-        pipe.fillColor = SKColor.redColor()
-        
-        pipe.strokeColor = SKColor.grayColor()
-        
-        pipe.position = CGPoint(x: frame.width / 2, y: frame.height / 2)
-        
-        addChild(pipe)
-        
-        
         //Setting up Physics
         self.physicsWorld.contactDelegate = self
         
@@ -82,6 +84,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player.position = CGPoint(x: CGRectGetMidX(self.frame) * 0.8, y: CGRectGetMidY(self.frame))
         
         addChild(player)
+        
+        
+        //Creating the Pipe's movement actions
+        movePipesAndRemove = SKAction()
+        
+        let distanceToMove = CGFloat(self.frame.size.width + 2.0 * pipeUp.frame.size.width)
+        
+        let movePipes = SKAction.moveByX(-distanceToMove, y:0.0, duration:NSTimeInterval(0.01 * distanceToMove))
+        
+        let removePipes = SKAction.removeFromParent()
+        
+        movePipesAndRemove = SKAction.sequence([movePipes, removePipes])
+        
+        
+        //Spawn the Pipes
+        let spawn = SKAction.runBlock({() in self.spawnPipes()})
+        
+        let delay = SKAction.waitForDuration(NSTimeInterval(2.0))
+        
+        let spawnThenDelay = SKAction.sequence([spawn, delay])
+        
+        let spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
+        
+        self.runAction(spawnThenDelayForever)
         
     }
     
@@ -133,5 +159,48 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
     }
     
+    
+    //MARK: - Function : spawn pipes
+    func spawnPipes() {
+        
+        let pipePair = SKNode()
+        
+        pipePair.position = CGPoint(x: self.frame.size.width + pipeDown.frame.size.width * 2, y: 0)
+        
+        pipePair.zPosition = -10
+        
+        let height = UInt32(self.frame.size.height / 4)
+        
+        let y = Double(arc4random_uniform(height) + height);
+        
+        
+        //Setting up Pipes
+        pipeDown = SKShapeNode(rectOfSize: CGSize(width: 80, height: 800), cornerRadius: 10)
+        
+        pipeDown.fillColor = SKColor.redColor()
+        
+        pipeDown.strokeColor = SKColor.grayColor()
+        
+        pipeDown.position = CGPoint(x: 0.0, y: y + Double(pipeDown.frame.size.height) + verticalPipeGap)
+        
+        pipePair.addChild(pipeDown)
+        
+        
+        pipeUp = SKShapeNode(rectOfSize: CGSize(width: 80, height: 800), cornerRadius: 10)
+        
+        pipeUp.position = CGPoint(x: 0.0, y: y)
+        
+        pipeUp.physicsBody = SKPhysicsBody(rectangleOfSize: pipeUp.frame.size)
+        
+        pipeUp.physicsBody?.dynamic = false
+        
+        pipePair.addChild(pipeUp)
+        
+        
+        //Setting up the contact        
+        pipePair.runAction(movePipesAndRemove)
+        pipes.addChild(pipePair)
+        
+    }
     
 }
